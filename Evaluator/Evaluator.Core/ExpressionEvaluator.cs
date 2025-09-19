b58397d5a558c0c3e2e25fbc0a8980cc4d83f7ba
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-19 15:16:07 -0500

    Basic Project Layout

---
+++ Evaluator/Evaluator.Core/ExpressionEvaluator.cs
@@ -0,0 +1,81 @@
+﻿namespace Evaluator.Core;
+    public class ExpressionEvaluator
+    {
+        public static double Evaluate(string infix)
+        {
+            var postfix = InfixToPostfix(infix);
+            Console.WriteLine(postfix);
+            return 0;
+            //return Calulate(postfix);
+        }
+
+        private static string InfixToPostfix(string infix)
+        {
+            var stack = new Stack<char>();
+            var postfix = string.Empty;
+            foreach (char item in infix)
+            {
+                if (IsOperator(item))
+                {
+                    if (item == ')')
+                    {
+                        do
+                        {
+                            postfix += stack.Pop();
+                        } while (stack.Peek() != '(');
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        if (stack.Count > 0)
+                        {
+                            if (PriorityInfix(item) > PriorityStack(stack.Peek()))
+                            {
+                                stack.Push(item);
+                            }
+                            else
+                            {
+                                postfix += stack.Pop();
+                                stack.Push(item);
+                            }
+                        }
+                        else
+                        {
+                            stack.Push(item);
+                        }
+                    }
+                }
+                else
+                {
+                    postfix += item;
+                }
+            }
+            while (stack.Count > 0)
+            {
+                postfix += stack.Pop();
+            }
+            return postfix;
+        }
+
+        private static bool IsOperator(char item) => item is '^' or '/' or '*' or '%' or '+' or '-' or '(' or ')';
+
+        private static int PriorityInfix(char op) => op switch
+        {
+            '^' => 4,
+            '*' or '/' or '%' => 2,
+            '-' or '+' => 1,
+            '(' => 5,
+            _ => throw new Exception("Invalid expression."),
+        };
+
+        private static int PriorityStack(char op) => op switch
+        {
+            '^' => 3,
+            '*' or '/' or '%' => 2,
+            '-' or '+' => 1,
+            '(' => 0,
+            _ => throw new Exception("Invalid expression."),
+        };
+
+    }
+
---
+++ Evaluator/Evaluator.UI.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using Evaluator.Core;
+
+Console.WriteLine("Hello, Evaluator");
+var infix = "4*5/(4+6)";
+var result = ExpressionEvaluator.Evaluate(infix);
+Console.WriteLine($"{infix}={result}");
