9585ae7eac45c294e2797fe3bf33046416666b76
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2025-09-19 16:12:18 -0500

    Updates Evaluator

--- Evaluator/Evaluator.UI.Console/Program.cs
+++ Evaluator/Evaluator.UI.Console/Program.cs
@@ -1,7 +1,16 @@
 ﻿
+
 using Evaluator.Core;
 
 Console.WriteLine("Hello, Evaluator");
-var infix = "4*5/(4+6)";
-var result = ExpressionEvaluator.Evaluate(infix);
-Console.WriteLine($"{infix}={result}");
+var infix1 = "4*5/(4+6)";
+var result1 = ExpressionEvaluator.Evaluate(infix1);
+Console.WriteLine($"{infix1}={result1}");
+
+var infix2 = "4*(5+6-(8/2^3)-7)-1";
+var result2 = ExpressionEvaluator.Evaluate(infix2);
+Console.WriteLine($"{infix2}={result2}");
+
+var infix3 = "123.89^(1.6/2.789)";
+var result3 = ExpressionEvaluator.Evaluate(infix3);
+Console.WriteLine($"{infix3}={result3}");
